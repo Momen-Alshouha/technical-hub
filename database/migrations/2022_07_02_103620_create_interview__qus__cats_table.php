274430fcb884f098bdb5da6c2d4899c480a96e03
momen-alshouha <momenalshouha@outlook.com>
2024-03-31 17:45:50 +0300

    seeders created and migration modified

--- database/migrations/2022_07_02_103620_create_interview__qus__cats_table.php
+++ database/migrations/2022_07_02_103620_create_interview__qus__cats_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -20,6 +21,38 @@ return new class extends Migration
             $table->text('image');
             $table->timestamps();
         });
+
+        // Insert records
+        DB::table('interview__qus__cats')->insert([
+            [
+                'title' => 'HTML',
+                'description' => 'HTML Interview Questions!',
+                'image' => 'html.png',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'Back-End',
+                'description' => 'Back-End Interview Questions!',
+                'image' => 'backend.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'Front-End',
+                'description' => 'Front-End Interview Questions!',
+                'image' => 'frontend.webp',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'Database',
+                'description' => 'Database Interview Questions',
+                'image' => 'database.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Review;
+use app\Models\Interview_Qus_Cat;
+use Carbon\Carbon;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,12 +19,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(9)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
 
+        User::create([
+            'name' => 'user User',
+            'email' => 'user@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => false,
+        ]);
+
+        // Get all user IDs
+        $userIds = User::pluck('id');
+
+        // Create 10 rviews
+        for ($i = 0; $i < 10; $i++) {
+            Review::create([
+                'user_id' => $userIds->random(),
+                'description' => 'This is a review description.',
+                'created_at' => Carbon::now()->subDays(rand(0, 30)),
+                'updated_at' => Carbon::now()->subDays(rand(0, 30)),
+            ]);
+        }
     }
 }
