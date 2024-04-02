4f853fee302fb62c72c197597039f66354785992
momen-alshouha <momenalshouha@outlook.com>
2024-04-02 17:39:53 +0300

    methods return types added

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Course;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class CourseController extends Controller
 {
@@ -13,7 +14,7 @@ class CourseController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index() : View
     {
         $courses = Course::all();
         return view('admin.courses', compact('courses'));
@@ -24,7 +25,7 @@ class CourseController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create() : View
     {
         return view('admin.add_course');
     }
@@ -35,21 +36,10 @@ class CourseController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request) : RedirectResponse
     {
         $course = Course::create($request->all());
         return redirect()->route('courses.index')->with('success', 'Course Added Succesfully');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Course  $course
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Course $course)
-    {
-        //
     }
 
     /**
@@ -58,7 +48,7 @@ class CourseController extends Controller
      * @param  \App\Models\Course  $course
      * @return \Illuminate\Http\Response
      */
-    public function edit(Course $course)
+    public function edit(Course $course) : View
     {
         $course = Course::findOrFail($course->id);
         return view('admin.edit_course', compact('course'));
@@ -86,7 +76,7 @@ class CourseController extends Controller
      * @param  \App\Models\Course  $course
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($id) : RedirectResponse
     {
         $course = Course::find($id);
         $course->delete();
