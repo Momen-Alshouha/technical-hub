33e4c6dbaa6ea56e3a903c1bd2ced07726e8a58f
momen-alshouha <momenalshouha@outlook.com>
2024-04-03 00:50:44 +0300

    user controller documented

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,29 +5,44 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-
-    public function index()
+    /**
+     * Display a listing of users.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function index(): View
     {
         $users = User::all();
         return view('admin.users', compact('users'));
     }
 
-    public function destroy(user $user)
+    /**
+     * Remove the specified user from storage.
+     *
+     * @param  \App\Models\User $user The user to be deleted
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy(User $user): RedirectResponse
     {
-        if($user->is_admin===1) {
-            if(User::checkIfLastAdminExists()) {
+        if ($user->is_admin === 1) {
+            if (User::checkIfLastAdminExists()) {
                 return redirect()->route('users')->with('message', 'Last admin can\'t be deleted!');
             }
         }
 
         $user->delete();
-        return redirect()->route('users')->with('message', 'user deleted successfully');
+        return redirect()->route('users')->with('message', 'User deleted successfully');
     }
 
+    /**
+     * Display the specified user profile.
+     *
+     * @param  int $id The ID of the user whose profile to display
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Contracts\View\View
+     */
     public function show($id)
     {
         $user = User::findOrFail($id); 
@@ -35,7 +50,6 @@ class UserController extends Controller
         if (auth()->user()->id !== $user->id) {
             return redirect()->route('home');
         }
-
         return view('user_profile', compact('user'));
     }
 }
