2e67d6cb27ff996d6fcc42a68379db5f116ad34c
momen-alshouha <momenalshouha@outlook.com>
2024-04-05 02:28:09 +0300

    modify admin dashboard index page to show total roadmaps and total roadmaps categories

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\RoadmapsCategory;
 use Illuminate\Http\Request;
 use App\Models\Course;
 use App\Models\Interview_Qus_Cat;
 use App\Models\Interview_Qustions;
 use App\Models\User;
 use App\Models\Review;
+use App\Models\Roadmap;
 use Illuminate\Contracts\View\View;
 
 class AdminController extends Controller
@@ -18,12 +20,16 @@ class AdminController extends Controller
         $totalInterviewQues = Interview_Qustions::count();
         $totalInterviewQuesCats = Interview_Qus_Cat::count();
         $totalCourses = Course::count();
+        $totalRoadmapsCategories = RoadmapsCategory::count();
+        $totalRoadmaps = Roadmap::count();
 
         return view('admin.index', [
             'totalCourses' => $totalCourses,
             'totalInterviewQuesCats' => $totalInterviewQuesCats,
             'totalInterviewQues' => $totalInterviewQues,
             'totalUsers' => $totalUsers,
+            'totalRoadmapsCategories'=>$totalRoadmapsCategories,
+            'totalRoadmaps'=>$totalRoadmaps,
         ]);
     }
 
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -40,6 +40,24 @@
                     </div>
                 </div>
             </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
+                <i class="fa fa-question fa-3x text-primary"></i>
+                    <div class="ms-3">
+                        <p class="mb-2">Roadmaps Categories</p>
+                        <h6 class="mb-0">{{$totalRoadmapsCategories}}</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
+                <i class="fa fa-question fa-3x text-primary"></i>
+                    <div class="ms-3">
+                        <p class="mb-2">Roadmaps</p>
+                        <h6 class="mb-0">{{$totalRoadmaps}}</h6>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
