23d0df96d1b7be6b41e8dc1373dbd54ae3fb89ff
momen-alshouha <momenalshouha@outlook.com>
2024-04-20 16:36:39 +0300

    Roadmap controller documented and contructor implemented to use auth middleware applied on all methods in the controller

--- app/Http/Controllers/Roadmap.php
+++ app/Http/Controllers/Roadmap.php
@@ -5,25 +5,55 @@ namespace App\Http\Controllers;
 use App\Models\Roadmap as ModelRoadmap;
 use App\Models\RoadmapsCategory;
 use App\Models\RoadmapStep;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class Roadmap extends Controller
 {
-    public function roadmapsCategories()
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display all roadmaps categories.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function roadmapsCategories() : View
     {
         $roadmapsCategories = RoadmapsCategory::all();
         return view('roadmaps_categories', compact('roadmapsCategories'));
     }
-    public function roadmapsByCategory($categoryId)
+
+    /**
+     * Display roadmaps belonging to a specific category.
+     *
+     * @param int $categoryId
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function roadmapsByCategory($categoryId) : View
     {
-        $roadmapsByCategory = ModelRoadmap::all()->where('cat_id',$categoryId);
+        $roadmapsByCategory = ModelRoadmap::where('cat_id', $categoryId)->get();
         $category = RoadmapsCategory::find($categoryId);
-        return view('roadmapsByCategory', compact('roadmapsByCategory','category'));
+        return view('roadmapsByCategory', compact('roadmapsByCategory', 'category'));
     }
 
-    public function roadmapSteps($roadmapId) {
-        $roadmapSteps = RoadmapStep::all()->where('roadmap_id',$roadmapId);
+    /**
+     * Display steps for a specific roadmap.
+     *
+     * @param int $roadmapId
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function roadmapSteps($roadmapId) : View
+    {
+        $roadmapSteps = RoadmapStep::where('roadmap_id', $roadmapId)->get();
         $roadmap = ModelRoadmap::find($roadmapId);
-        return view('roadmap_steps', compact('roadmapSteps','roadmap'));
+        return view('roadmap_steps', compact('roadmapSteps', 'roadmap'));
     }
 }
